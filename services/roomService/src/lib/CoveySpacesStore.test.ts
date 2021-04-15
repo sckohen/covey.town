2e26217c3aadbea31b0699b90d0bafd6cf435181
Can Kohen <kohen.s@northeastern.edu>
2021-04-14 20:34:15 -0400

    Tim added tests (uncommented existing tests)
    Co-authored-by: TimTianqiZhang <TimTianqiZhang@users.noreply.github.com>

--- services/roomService/src/lib/CoveySpacesStore.test.ts
+++ services/roomService/src/lib/CoveySpacesStore.test.ts
@@ -1,89 +1,72 @@
- import { nanoid } from 'nanoid';
- import CoveyTownsStore from './CoveyTownsStore';
- import CoveyTownListener from '../types/CoveyTownListener';
- import Player from '../types/Player';
-
-//  const mockCoveyListenerTownDestroyed = jest.fn();
-//  const mockCoveyListenerOtherFns = jest.fn();
-
-//  function mockCoveyListener(): CoveyTownListener {
-//    return {
-//      onPlayerDisconnected(removedPlayer: Player): void {
-//        mockCoveyListenerOtherFns(removedPlayer);
-//      },
-//      onPlayerMoved(movedPlayer: Player): void {
-//        mockCoveyListenerOtherFns(movedPlayer);
-//      },
-//      onTownDestroyed() {
-//        mockCoveyListenerTownDestroyed();
-//      },
-//      onPlayerJoined(newPlayer: Player) {
-//        mockCoveyListenerOtherFns(newPlayer);
-//      },
-//    };
-//  }
-
-//  function createTownForTesting(friendlyNameToUse?: string, isPublic = false) {
-//    const friendlyName = friendlyNameToUse !== undefined ? friendlyNameToUse :
-//      `${isPublic ? 'Public' : 'Private'}TestingTown=${nanoid()}`;
-//    return CoveyTownsStore.getInstance()
-//      .createTown(friendlyName, isPublic);
-//  }
+import { nanoid } from 'nanoid';
+import CoveyTownsStore from './CoveyTownsStore';
+import CoveyTownListener from '../types/CoveyTownListener';
+import Player from '../types/Player';
+import CoveySpacesStore from './CoveySpacesStore';
 
 
-// describe('CoveyTownsStore', () => {
-//   beforeEach(() => {
-//     mockCoveyListenerTownDestroyed.mockClear();
-//     mockCoveyListenerOtherFns.mockClear();
-//   });
-//   it('should be a singleton', () => {
-//     const store1 = CoveyTownsStore.getInstance();
-//     const store2 = CoveyTownsStore.getInstance();
-//     expect(store1)
-//       .toBe(store2);
-//   });
+function createTownForTesting(friendlyNameToUse?: string, isPublic = false) {
+    const friendlyName = friendlyNameToUse !== undefined ? friendlyNameToUse :
+      `${isPublic ? 'Public' : 'Private'}TestingTown=${nanoid()}`;
+    return CoveyTownsStore.getInstance()
+      .createTown(friendlyName, isPublic);
+  }
 
-//   describe('createTown', () => {
-//     it('Should allow multiple towns with the same friendlyName', () => {
-//       const firstTown = createTownForTesting();
-//       const secondTown = createTownForTesting(firstTown.friendlyName);
-//       expect(firstTown)
-//         .not
-//         .toBe(secondTown);
-//       expect(firstTown.friendlyName)
-//         .toBe(secondTown.friendlyName);
-//       expect(firstTown.coveyTownID)
-//         .not
-//         .toBe(secondTown.coveyTownID);
-//     });
-//   });
+  function createSpaceForTesting(spaceID: string, townControllerID: string) {
+    return CoveySpacesStore.getInstance()
+      .createSpace(spaceID, townControllerID);
+  }
 
-//   describe('getControllerForTown', () => {
-//     it('Should return the same controller on repeated calls', async () => {
-//       const firstTown = createTownForTesting();
-//       expect(firstTown)
-//         .toBe(CoveyTownsStore.getInstance()
-//           .getControllerForTown(firstTown.coveyTownID));
-//       expect(firstTown)
-//         .toBe(CoveyTownsStore.getInstance()
-//           .getControllerForTown(firstTown.coveyTownID));
-//     });
-//   });
 
-//   describe('updateTown', () => {
-//     it('Should check the password before updating any value', () => {
-//       const town = createTownForTesting();
-//       const { friendlyName } = town;
-//       const res = CoveyTownsStore.getInstance()
-//         .updateTown(town.coveyTownID, 'abcd', 'newName', true);
-//       expect(res)
-//         .toBe(false);
-//       expect(town.friendlyName)
-//         .toBe(friendlyName);
-//       expect(town.isPubliclyListed)
-//         .toBe(false);
+ describe('CoveyTownsStore', () => {
+   beforeEach(() => {
+   });
+   it('should be a singleton', () => {
+     const store1 = CoveySpacesStore.getInstance();
+     const store2 = CoveySpacesStore.getInstance();
+     expect(store1)
+       .toBe(store2);
+   });
 
-//     });
+   describe('createSpace', () => {
+     it('Should check if the town exist or not before creating the space', () => {
+       const firstTown = createTownForTesting();
+       const firstSpace = createSpaceForTesting('1','1');
+       const secondSpace = createSpaceForTesting('2', firstTown.coveyTownID);
+       expect(firstSpace).toThrowError();
+       expect(secondSpace.coveySpaceID).toBe(`${firstTown.coveyTownID}_${secondSpace.coveySpaceID}`)
+     });
+   });
+
+   describe('getControllerForSpace', () => {
+     it('Should return the same controller on repeated calls', async () => {
+       const firstTown = createTownForTesting();
+       const firstSpace = createSpaceForTesting('1', firstTown.coveyTownID);
+       expect(firstSpace)
+         .toBe(CoveySpacesStore.getInstance()
+           .getControllerForSpace(firstSpace.coveySpaceID));
+       expect(firstSpace)
+         .toBe(CoveySpacesStore.getInstance()
+           .getControllerForSpace(firstSpace.coveySpaceID));
+     });
+   });
+
+   describe('updateSpace', () => {
+     it('Should check if the space controller exists before updating', () => {
+       const town = createTownForTesting();
+       const space = createSpaceForTesting('1', town.coveyTownID);
+       const { friendlyName } = town;
+       const res = CoveySpacesStore.getInstance()
+         .updateSpace('1', 'abcd', 'newName');
+       CoveySpacesStore.getInstance()
+         .updateSpace(space.coveySpaceID, 'abcd', 'newName');
+       expect(res)
+         .toBe(false);
+       expect(space.spaceHostID)
+        .toBe('abcd');
+      expect(space.presenterID)
+        .toBe('newName');
+     });
 //     it('Should fail if the townID does not exist', async () => {
 //       const town = createTownForTesting();
 //       const { friendlyName } = town;
@@ -97,7 +80,7 @@
 //       expect(town.isPubliclyListed)
 //         .toBe(false);
 
-//     });
+     });
 //     it('Should update the town parameters', async () => {
 
 //       // First try with just a visiblity change
@@ -133,50 +116,49 @@
 //       expect(town.friendlyName)
 //         .toBe(friendlyName);
 //     });
-//   });
+   });
 
-//   describe('deleteTown', () => {
-//     it('Should check the password before deleting the town', () => {
-//       const town = createTownForTesting();
-//       const res = CoveyTownsStore.getInstance()
-//         .deleteTown(town.coveyTownID, `${town.townUpdatePassword}*`);
-//       expect(res)
-//         .toBe(false);
-//     });
-//     it('Should fail if the townID does not exist', async () => {
-//       const res = CoveyTownsStore.getInstance()
-//         .deleteTown('abcdef', 'efg');
-//       expect(res)
-//         .toBe(false);
-//     });
-//     it('Should disconnect all players', async () => {
-//       const town = createTownForTesting();
-//       town.addTownListener(mockCoveyListener());
-//       town.addTownListener(mockCoveyListener());
-//       town.addTownListener(mockCoveyListener());
-//       town.addTownListener(mockCoveyListener());
-//       town.disconnectAllPlayers();
+  describe('disband space', () => {
+    // it('Should check the password before deleting the town', () => {
+    //   const town = createTownForTesting();
+    //   const res = CoveyTownsStore.getInstance()
+    //     .deleteTown(town.coveyTownID, `${town.townUpdatePassword}*`);
+    //   expect(res)
+    //     .toBe(false);
+    // });
+    it('Should fail if the spaceID does not exist', async () => {
+      const res = CoveySpacesStore.getInstance()
+        .disbandSpace('abcdef', 'player1');
+      expect(res)
+        .toBe(false);
+    });
+  //   it('Should publicize the space', async () => {
+  //     const town = createTownForTesting();
+  //     const space = createSpaceForTesting('1', town.coveyTownID);
+  //     const res = CoveySpacesStore.getInstance()
+  //       .disbandSpace(space.coveySpaceID);
 
-//       expect(mockCoveyListenerOtherFns.mock.calls.length)
-//         .toBe(0);
-//       expect(mockCoveyListenerTownDestroyed.mock.calls.length)
-//         .toBe(4);
-//     });
-//   });
+  //     expect(space.)
+  //       .toBe(0);
+  //     expect(mockCoveyListenerTownDestroyed.mock.calls.length)
+  //       .toBe(4);
+  //   });
+   });
 
-//   describe('listTowns', () => {
-//     it('Should include public towns', async () => {
-//       const town = createTownForTesting(undefined, true);
-//       const towns = CoveyTownsStore.getInstance()
-//         .getTowns();
-//       const entry = towns.filter(townInfo => townInfo.coveyTownID === town.coveyTownID);
-//       expect(entry.length)
-//         .toBe(1);
-//       expect(entry[0].friendlyName)
-//         .toBe(town.friendlyName);
-//       expect(entry[0].coveyTownID)
-//         .toBe(town.coveyTownID);
-//     });
+  describe('listSpaces', () => {
+    it('Should include all spaces', async () => {
+      const town = createTownForTesting(undefined, true);
+      createSpaceForTesting('1', town.coveyTownID);
+      createSpaceForTesting('2', town.coveyTownID);
+      const spaces = CoveySpacesStore.getInstance()
+        .listSpaces();
+      expect(spaces.length)
+        .toBe(2);
+      // expect(entry[0].friendlyName)
+      //   .toBe(town.friendlyName);
+      // expect(entry[0].coveyTownID)
+      //   .toBe(town.coveyTownID);
+    });
 //     it('Should include each CoveyTownID if there are multiple towns with the same friendlyName', async () => {
 //       const town = createTownForTesting(undefined, true);
 //       const secondTown = createTownForTesting(town.friendlyName, true);
@@ -241,4 +223,4 @@
 //     });
 //   });
 // });
-
+  });
